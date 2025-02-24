96cd029d4a4af69e3520b9609d9f584a98bb0cc2
Shakil Ahmad <ahmadshakil003@gmail.com>
2025-02-24 23:23:44 +0500

    POS Product Module API CRUD Completed

--- pos_api/pos_api/settings.py
+++ pos_api/pos_api/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
+    'rest_framework', # Rest Framework
+    'rest_framework_simplejwt', # RestFramework Json Web Token JWT
     'user',  # Our user app
+    'product', # Product app
 ]
 
 MIDDLEWARE = [
--- pos_api/pos_api/urls.py
+++ pos_api/pos_api/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/user/', include('user.urls')),
+    path('api/product/', include('product.urls')),
 ]
 
